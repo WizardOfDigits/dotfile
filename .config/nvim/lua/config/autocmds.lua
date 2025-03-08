c80b6ddd53bfc1511b1ea76b30c0c606fa7a4849
WizardOfDigits <saurav.stamang@gmail.com>
2025-03-08 19:29:40 +0545

    add features to use .ejs extendsion as html

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 -- ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝  ╚═════╝╚═╝     ╚═╝╚═════╝ ╚══════╝
 
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Set .ejs files to use HTML filetype
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.ejs",
+  callback = function()
+    vim.bo.filetype = "html" -- Treat as HTML
+  end,
+})
